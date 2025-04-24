ea8cd7a9165464feff90abab409ae3f149c347b3
Eliab <eliabsiji@gmail.com>
2025-04-24 10:24:19 +0100

    sitemap stuffs

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://worldthunderministry.org', // ← Replace with your domain
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+  }
+  
